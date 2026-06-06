69459e99d9a61f8f22e86b7779fe43f68ef1b20b
Robin2883 <bluebillu83@gmail.com>
2026-06-06 12:51:08 +0600

    svc linear classifier added

---
+++ SVC/svc.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#dataset loaded
+df=pd.read_csv('Social_Network_Ads.csv')
+df.head()
+
+df.info()
+df.describe()
+df.isnull().sum()
+
+#separating independent & dependent variables
+X=df.iloc[:, :-1].values
+y=df.iloc[:, -1].values
+
+#splitting train & test data
+X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=42)
+
+#scaling features
+sc=StandardScaler()
+X_train_scaled=sc.fit_transform(X_train)
+X_test_scaled=sc.transform(X_test)
+print(X_train_scaled)
+print(X_test_scaled)
+
+#training model
+regr=SVC(kernel='linear', random_state=42)
+regr.fit(X_train_scaled, y_train)
+
+#prediction 
+y_pred=regr.predict(X_test_scaled) #'SVC' has no attribute 'predict_proba'
+print(y_pred)
+
+accuracy=accuracy_score(y_test, y_pred)
+accuracy
+
+confusion=confusion_matrix(y_test, y_pred)
+sns.heatmap(confusion, annot=True)
+plt.xlabel('Prediction')
+plt.ylabel('True')
+plt.title('Confusion Matrix')
+plt.savefig('svc_linear_conf_matrix.png')
+plt.show()
+
+regr.predict([[27, 80000]])
